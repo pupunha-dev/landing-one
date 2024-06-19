762b3fabc9d4b1be62ff0369e3b3179725b97fe9
PupunhaDev <pupunha.dev@gmail.com>
2024-06-19 00:54:08 -0300

    shadcn/ui installed and Button component added to UI components.

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,11 +1,18 @@
+import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <main>
-      <Link href="/saas">SaaS</Link>
-      <Link href="/blog">Blog</Link>
-      <Link href="/joinus">Join Us</Link>
+      <Button asChild>
+        <Link href="/saas">SaaS</Link>
+      </Button>
+      <Button asChild>
+        <Link href="/blog">Blog</Link>
+      </Button>
+      <Button asChild>
+        <Link href="/joinus">Join Us</Link>
+      </Button>  
     </main>
   );
 }
